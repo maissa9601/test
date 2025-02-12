d241f448b981847b609b20b4b594926b91fdfa17
maissa9601 <maissak367@gmail.com>
2025-02-12 10:56:22 +0100

    Premier commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { JobOfferFormComponent } from './components/job-offer-form/job-offer-form.component';
+import { JobOfferListComponent } from './components/job-offer-list/job-offer-list.component';
+import { JobOfferUpdateComponent } from './components/job-offer-update/job-offer-update.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  { path: 'offer', component: JobOfferFormComponent },
+  { path: '', component: JobOfferListComponent },
+  {path: ':offer/:id', component: JobOfferUpdateComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
